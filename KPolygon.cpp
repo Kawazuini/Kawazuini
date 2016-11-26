3c499c6b1590316eabe90eb70bd6bfdecb302bea
前田 卓魅 <kawazuini@gmail.com>
2016-11-26 22:02:32 +0900

    revise collision

--- KPolygon.cpp
+++ KPolygon.cpp
@@ -5,6 +5,7 @@
  */
 #include "KPolygon.h"
 
+#include "KMath.h"
 #include "KSegment.h"
 
 KPolygon::KPolygon(const List<KVector>& aVertex) {
@@ -15,8 +16,8 @@ KPolygon::KPolygon(const List<KVector>& aVertex) {
 bool KPolygon::operator*(const KVector& aPoint) {
     // 交差点と頂点との外積がすべて等しい場合衝突(交差点がポリゴン内にある)
     for (auto i = mVertex.begin(), i_e = mVertex.end(); i != i_e; ++i) {
-        KVector ver = (i + 1 == i_e) ? *(mVertex.begin()) : *i;
-        if (mNormal != (ver - *i).cross(aPoint - ver)) return false;
+        KVector ver = (i + 1 == i_e) ? *(mVertex.begin()) : *(i + 1);
+        if (mNormal != (ver - *i).cross(aPoint - ver).normalization()) return false;
     }
     return true;
 }
@@ -27,11 +28,11 @@ bool KPolygon::operator*(const KSegment& aSegment) {
 
 KVector KPolygon::hitPoint(const KSegment& aSegment) {
     // 線分と面との衝突判定(線分の端がそれぞれ面の表裏に存在するか)
-    if (aSegment.mVec1.dot(mNormal) * aSegment.mVec2.dot(mNormal) < 0) {
-        KVector v1 = aSegment.mVec1 - mVertex[0];
-        KVector v2 = aSegment.mVec2 - mVertex[0];
-        float m = v1.dot(mNormal);
-        float n = v2.dot(mNormal);
+    float m = (aSegment.mVec1 - mVertex[0]).dot(mNormal);
+    float n = (aSegment.mVec2 - mVertex[0]).dot(mNormal);
+    if (m * n < 0) {
+        m = Math::abs(m);
+        n = Math::abs(n);
         return (aSegment.mVec2 - aSegment.mVec1) * m / (m + n) + aSegment.mVec1;
     }
     return KVector(); // 衝突していないときは零ベクトルを返す
