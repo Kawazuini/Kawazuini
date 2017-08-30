29c93e38a3572e7cdfedb49ba292138962df7834
前田 卓魅 <kawazuini@gmail.com>
2017-08-30 19:04:24 +0900

    light synchronized with the camera

--- KHandLight.cpp
+++ KHandLight.cpp
@@ -5,12 +5,18 @@
  */
 #include "KHandLight.h"
 
+#include "KCamera.h"
+
 KHandLight::KHandLight(
         const KVector& aPosition,
         const KVector& aDirection
         ) :
 mHandPosition(aPosition),
 mHandDirection(aDirection) {
+}
+
+KHandLight::KHandLight(const KCamera& aCamera) :
+KHandLight(aCamera.position(), aCamera.direction()) {
 }
 
 void KHandLight::update() {
--- KHandLight.h
+++ KHandLight.h
@@ -9,6 +9,8 @@
 #include "KLight.h"
 #include "KUpdater.h"
 
+class KCamera;
+
 /**
  * @brief  \~english  handy light
  * @brief  \~japanese ハンドライト
@@ -19,10 +21,21 @@ private:
     /* 同期用位置ベクトル */ const KVector& mHandPosition;
     /* 同期用方向ベクトル */ const KVector& mHandDirection;
 public:
+    /**
+     * \~english
+     * @param aPosition  synchronization position  vector
+     * @param aDirection synchronization direction vector
+     * \~japanese
+     * @param aPosition  同期用位置ベクトル
+     * @param aDirection 同期用方向ベクトル
+     */
     KHandLight(
             const KVector& aPosition,
             const KVector& aDirection
             );
+    /// \~english  @param synchronization camera
+    /// \~japanese @param 同期用カメラ
+    KHandLight(const KCamera& aCamera);
     virtual ~KHandLight() = default;
 
     void update() override;
